18e068b47ccadb3576c728292b5edd7f98064b29
Aidas-Baublys <aidas.baublys.darbo@gmail.com>
2021-01-24 19:21:10 +0200

    Initial commit, project pre-production.

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import { Button } from "./components";
+
+import "./index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Here goes our app</h1>
+      <Button>Example Button</Button>
+    </div>
+  );
+}
+
+export default App;
